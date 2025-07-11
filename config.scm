68f9b5e68c5b522f4dedb27eb0d5234063caaecc
averagedeveloper <mario.perry@protonmail.com>
2025-07-11 01:11:43 -0400

    Update config.scm
    fixed formatting

--- config.scm
+++ config.scm
@@ -4,9 +4,9 @@
 (use-service-modules base cups networking ssh docker)
 
 (operating-system
-  ;; Use long-term support kernel with additional boot parameters
+  ;; Use long-term support kernel with additional boot arguments
   (kernel linux-lts
-          (kernel-parameters
+          (kernel-arguments
            (list
             "quiet"                        ;; Minimal boot messages
             "noatime"                      ;; Don't update access time on file reads
@@ -14,7 +14,7 @@
             "cgroup_enable=memory"        ;; Required for Docker memory control
             "swapaccount=1"               ;; Enable cgroup swap accounting
             "intel_iommu=on"              ;; Enable Intel IOMMU for device isolation
-            "iommu=pt"))                 ;; Use passthrough mode for direct device access
+            "iommu=pt")))                 ;; Use passthrough mode for direct device access
 
   ;; Nonfree firmware blobs (Wi-Fi, graphics, etc.)
   (firmware (list linux-firmware))
@@ -46,23 +46,22 @@
   (services
    (list
     (service docker-service-type)          ;; Enable Docker service
-    (service openssh-service-type)        ;; Enable SSH server
-    (service ntp-service-type)            ;; Clock sync via NTP
+    (service openssh-service-type)         ;; Enable SSH server
+    (service ntp-service-type)             ;; Clock sync via NTP
 
-    ;; Static network configuration (replace "eno1" with your real interface)
+    ;; Static network configuration (replace "eth0" with your real interface)
     (service static-networking-service-type
      (list
       (static-networking
        (addresses
         (list (network-address
-            (device "eth0")
-            (value "192.168.1.100/24"))))
-         (routes
-          (list (network-route
-            (destination "default")
-            (gateway "192.168.1.1"))))
-          (name-servers '("1.1.1.1" "8.8.8.8"))))
-
+               (device "eth0")
+               (value "192.168.1.100/24"))))
+       (routes
+        (list (network-route
+               (destination "default")
+               (gateway "192.168.1.1"))))
+       (name-servers '("1.1.1.1" "8.8.8.8")))))
 
     (service cups-service-type)           ;; Printer server (optional)
     (service zram-device-service-type     ;; RAM-compressed swap device
@@ -78,45 +77,44 @@
 
   ;; Filesystem definitions
   (file-systems
-   (append
-    (list
-     ;; Root partition (read-only, minimal system)
-     (file-system
-      (device (label "SYSTEM"))
-      (mount-point "/")
-      (type "ext4")
-      (flags '(read-only)))
+   (cons*
+    ;; Root partition (read-only, minimal system)
+    (file-system
+     (device (label "SYSTEM"))
+     (mount-point "/")
+     (type "ext4")
+     (flags '(read-only)))
 
-     ;; EFI partition for GRUB bootloader
-     (file-system
-      (device (label "EFI"))
-      (mount-point "/boot/efi")
-      (type "vfat"))
+    ;; EFI partition for GRUB bootloader
+    (file-system
+     (device (label "EFI"))
+     (mount-point "/boot/efi")
+     (type "vfat"))
 
-     ;; Persistent partition to store /var, /home, /etc
-     (file-system
-      (device (label "DATA"))
-      (mount-point "/persist")
-      (type "ext4"))
+    ;; Persistent partition to store /var, /home, /etc
+    (file-system
+     (device (label "DATA"))
+     (mount-point "/persist")
+     (type "ext4"))
 
-     ;; Bind-mount persistent paths into standard FHS locations
-     (file-system
-      (device "/persist/etc")
-      (mount-point "/etc")
-      (type "none")
-      (flags '(bind-mount)))
+    ;; Bind-mount persistent paths into standard FHS locations
+    (file-system
+     (device "/persist/etc")
+     (mount-point "/etc")
+     (type "none")
+     (flags '(bind-mount)))
 
-     (file-system
-      (device "/persist/var")
-      (mount-point "/var")
-      (type "none")
-      (flags '(bind-mount)))
+    (file-system
+     (device "/persist/var")
+     (mount-point "/var")
+     (type "none")
+     (flags '(bind-mount)))
 
-     (file-system
-      (device "/persist/home")
-      (mount-point "/home")
-      (type "none")
-      (flags '(bind-mount))))
-    
+    (file-system
+     (device "/persist/home")
+     (mount-point "/home")
+     (type "none")
+     (flags '(bind-mount)))
+
     ;; Add essential pseudo-filesystems like /proc, /sys, /dev
     %base-file-systems)))
