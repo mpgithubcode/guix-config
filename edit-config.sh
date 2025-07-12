cea548aa2a09e90704c934ea46c8be24a1902e99
averagedeveloper <mario.perry@protonmail.com>
2025-07-11 22:32:49 -0400

    Update edit-config.sh

--- edit-config.sh
+++ edit-config.sh
@@ -10,16 +10,23 @@ fi
 
 # Prompt for new values
 read -rp "Enter new username (current: alice): " NEW_USER
-read -rp "Enter new static IP (current: 192.168.1.100/24): " NEW_IP
+read -rp "Enter new static IP (e.g., 192.168.1.100): " NEW_IP_RAW
 read -rp "Enter new ZRAM size (e.g., 2G, current: 1G): " NEW_ZRAM
 
 # Apply changes using sed (with backup)
 cp "$CONFIG_FILE" "${CONFIG_FILE}.bak"
 
+# Append /24 if not present
+if [[ "$NEW_IP_RAW" == */* ]]; then
+  NEW_IP="$NEW_IP_RAW"
+else
+  NEW_IP="${NEW_IP_RAW}/24"
+fi
+
 sed -i \
   -e "s/(name \"[^\"]*\")/(name \"$NEW_USER\")/" \
   -e "s/(home-directory \"\/home\/[^\"]*\")/(home-directory \"\/home\/$NEW_USER\")/" \
-  -e "s/(value \"[0-9.\/]*\")/(value \"$NEW_IP\")/" \
+  -e "s|(value \"[0-9./]*\")|(value \"$NEW_IP\")|" \
   -e "s/(size \"[0-9A-Za-z]*\")/(size \"$NEW_ZRAM\")/" \
   "$CONFIG_FILE"
 
