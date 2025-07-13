cec3d7738b7670b31cb197d3b5ee75442e37dc59
averagedeveloper <mario.perry@protonmail.com>
2025-07-13 09:39:09 -0400

    Update make-folders.sh

--- make-folders.sh
+++ make-folders.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+# Replace these with actual partition names
+SYSTEM_DEV=/dev/nvme0n1p1    # ext4 root partition
+EFI_DEV=/dev/nvme0n1p2       # vfat EFI partition
+DATA_DEV=/dev/nvme0n1p3      # optional data partition
+
+# Label the partitions
+e2label $SYSTEM_DEV SYSTEM
+fatlabel $EFI_DEV EFI
+e2label $DATA_DEV DATA
+
+# Trigger udev to update labels
+udevadm trigger
+udevadm settle
+
+# Mount the filesystems
+mount $SYSTEM_DEV /mnt
+mkdir -p /mnt/boot/efi
+mount $EFI_DEV /mnt/boot/efi
+mkdir -p /mnt/persist
+mount $DATA_DEV /mnt/persist
+
+# Create base directories
 mkdir -p /mnt/{gnu,var,etc,home,tmp}
 chmod 0555 /mnt/gnu
 chmod 0755 /mnt/var /mnt/etc /mnt/home
