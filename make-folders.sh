36f575f0d4b9fc02dc00185445daa82e7d2f7859
averagedeveloper <mario.perry@protonmail.com>
2025-07-13 09:46:41 -0400

    Update make-folders.sh

--- make-folders.sh
+++ make-folders.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+USERNAME=mperry
 # Replace these with actual partition names
 SYSTEM_DEV=/dev/nvme0n1p1    # ext4 root partition
 EFI_DEV=/dev/nvme0n1p2       # vfat EFI partition
 DATA_DEV=/dev/nvme0n1p3      # optional data partition
+
+# Check devices before use
+for dev in $SYSTEM_DEV $EFI_DEV $DATA_DEV; do
+  if [ ! -b "$dev" ]; then
+    echo "Device $dev not found!"
+    exit 1
+  fi
+done
+
 
 # Label the partitions
 e2label $SYSTEM_DEV SYSTEM
@@ -21,9 +31,13 @@ mount $EFI_DEV /mnt/boot/efi
 mkdir -p /mnt/persist
 mount $DATA_DEV /mnt/persist
 
-# Create base directories
-mkdir -p /mnt/{gnu,var,etc,home,tmp}
+# Create base directories including the user home directory
+mkdir -p /mnt/{gnu,var,etc,home,home/$USERNAME,tmp}
+
 chmod 0555 /mnt/gnu
-chmod 0755 /mnt/var /mnt/etc /mnt/home
+chmod 0755 /mnt/var /mnt/etc /mnt/home /mnt/home/$USERNAME
 chmod 1777 /mnt/tmp
+chown $USERNAME:$USERNAME /mnt/home/$USERNAME
+
+# Make sure ownership is root for system dirs; you might want to set user ownership on home dir later
 chown root:root /mnt/gnu /mnt/var /mnt/etc /mnt/home /mnt/tmp
